1b2442dfd25ac75548601b718f392eefe37f7bc7
ShravyaHub <shravyak710@gmail.com>
2020-11-01 15:54:19 +0530

    ADDED: hotel with name and rates for a regular customer

---
+++ src/main/java/HotelDetails.java
@@ -0,0 +1,26 @@
+public class HotelDetails {
+
+    private String hotelName;
+    private int regularRate;
+
+    public HotelDetails(String hotelName, int regularRate) {
+        this.hotelName = hotelName;
+        this.regularRate = regularRate;
+    }
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public int getRegularRate() {
+        return regularRate;
+    }
+
+    public void setRegularRate(int regularRate) {
+        this.regularRate = regularRate;
+    }
+}
